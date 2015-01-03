9d45aaf4bbed573a8656266332fbc6569d97875c
Janne Saarela <janne.juhani@gmail.com>
2015-01-03 15:15:51 +0200

    Add more default options after theme switch, set page for posts and page, insert blog-page to posts if it does not exist, add blog to default navigation if the page was inserted in theme switch

--- library/theme-default-options.php
+++ library/theme-default-options.php
@@ -3,7 +3,57 @@
 add_action('after_switch_theme', 'degrona15_setup_options', 100 );
 
 function degrona15_setup_options() {
-  if ( ! has_nav_menu( 'top-bar-l' ) ) {
+
+  // update_option('permalink_structure', '/%year%/%monthnum%/%day%/%postname%/' );
+
+  $hello_world = get_post( 1, 'ARRAY_A' );
+  // By default we check only english and finnish versions of default post/page
+  if( $hello_world && $hello_world['post_title'] == 'Hello World!' || $hello_world['post_title'] == 'Moikka maailma!') :
+    wp_update_post(
+      array (
+            'ID'            => 1,
+            'post_title'    => 'Tiedote: Tervetuloa käyttämään WordPressiä!',
+            'post_name'     => 'tiedote-tervetuloa-kayttamaan-wordpressia',
+            'post_content'  => 'Tämä on sivuston esimerkkiartikkeli. Artikkeleita on hyvä käyttää mm. blogitekstien julkaisemiseen. Poista tämä teksti hallintapaneelista -> Artikkelit -> Kaikki artikkelit -> valitse "Tiedote: Tervetuloa käyttämään WordPressiä!"-artikkeli ja siirrä se roskakoriin. Voit kirjoittaa uusia artikkeleita valitsemalla "Lisää uusi" Artikkelit-sivulla.'
+      ) );
+  endif;
+
+  $sample_page = get_post( 2, 'ARRAY_A' );
+  // By default we check only english and finnish versions of default post/page
+  if( $sample_page && $sample_page['post_title'] == 'Sample Page' || $sample_page['post_title'] == 'Esimerkkisivu') :
+    wp_update_post(
+      array (
+            'ID'            => 2,
+            'post_title'    => 'Etusivu',
+            'post_name'     => 'etusivu',
+            'post_content'  => ''
+      ) );
+    update_option( 'page_on_front', 2 );
+    update_option( 'show_on_front', 'page' );
+
+  endif;
+
+
+  $posts_page_id = get_option( 'page_for_posts' );
+  $blog_page_already_exists = false;
+
+  if( ! $posts_page_id && ! get_page_by_path( 'blogi' )) :
+
+    $posts_page_id = wp_insert_post( array('post_title' => __('Blog', 'DeGrona15'), 'post_name' => __('blog', 'DeGrona15'), 'post_status' => 'publish', 'post_type' => 'page' ) );
+
+    if( $posts_page_id ) :
+
+      update_option( 'page_for_posts', $posts_page_id );
+
+    endif;
+
+  else :
+
+    $blog_page_already_exists = true;
+
+  endif;
+
+  if ( ! has_nav_menu( 'top-bar-l' ) ) :
 
     $menus = get_terms( 'nav_menu', array( 'hide_empty' => true ) );
 
@@ -11,12 +61,32 @@ function degrona15_setup_options() {
 
       $menu_id = wp_create_nav_menu( __( 'The Main Menu', 'DeGrona15' ) );
 
-      // Set up default menu items
-      wp_update_nav_menu_item( $menu_id, 0, array(
-        'menu-item-title' =>  __('Home', 'DeGrona15'),
-        'menu-item-url' => home_url( '/' ),
-        'menu-item-status' => 'publish')
-      );
+      /**
+      * Set up default menu items
+      * If posts page is set above: add blog-page to menu as well
+      */
+
+        wp_update_nav_menu_item( $menu_id, 0,
+          array(
+            'menu-item-title' =>  __('Home', 'DeGrona15'),
+            'menu-item-url' => home_url( '/' ),
+            'menu-item-status' => 'publish'
+          )
+        );
+
+        if( $posts_page_id && ! $blog_page_already_exists ){
+
+          wp_update_nav_menu_item( $menu_id, 0,
+            array(
+              'menu-item-title' =>  __('Blog', 'DeGrona15'),
+              'menu-item-object-id' => $posts_page_id,
+              'menu-item-object' => 'page',
+              'menu-item-type'      => 'post_type',
+              'menu-item-status' => 'publish'
+            )
+          );
+      }
+
       // Add menu to mobile and top-bar-l
       $menu_defaults = array(
         'mobile-off-canvas' => $menu_id,
@@ -34,40 +104,6 @@ function degrona15_setup_options() {
 
       set_theme_mod('nav_menu_locations', $menu_defaults);
     }
-
-  }
-
-  // update_option('permalink_structure', '/%year%/%monthnum%/%day%/%postname%/' );
-
-  // Check if theme is switched first time or not
-  $first_theme_switch = get_option( 'degrona15_theme_switched' );
-
-  if ( ! $first_theme_switch ) :
-    // This is first switch to theme: let's edit default post/page
-
-    $hello_world = get_post( 1, 'ARRAY_A' );
-
-    if( $hello_world && $hello_world['post_title'] == 'Hello World!' || $hello_world['post_title'] == 'Moikka maailma!') :
-      wp_update_post(
-        array (
-              'ID'            => 1,
-              'post_title'    => 'Tiedote: Tervetuloa käyttämään WordPressiä!',
-              'post_content'  => 'Tämä on sivuston esimerkkiartikkeli. Artikkeleita on hyvä käyttää mm. blogitekstien julkaisemiseen. Poista tämä teksti hallintapaneelista -> Artikkelit -> Kaikki artikkelit -> valitse "Tiedote: Tervetuloa käyttämään WordPressiä!"-artikkeli ja siirrä se roskakoriin. Voit kirjoittaa uusia artikkeleita valitsemalla "Lisää uusi" Artikkelit-sivulla.'
-        ) );
-    endif;
-
-    $sample_page = get_post( 2, 'ARRAY_A' );
-
-    if( $sample_page && $sample_page['post_title'] == 'Sample Page' || $sample_page['post_title'] == 'Esimerkkisivu') :
-      wp_update_post(
-        array (
-              'ID'            => 2,
-              'post_title'    => 'Etusivu',
-              'post_content'  => ''
-        ) );
-    endif;
-
-    update_option( 'degrona15_theme_switched', true );
 
   endif;
 
