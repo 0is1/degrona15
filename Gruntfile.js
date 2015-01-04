91b09dea2151ff0d26c60d307b61a4077d02f582
Janne Saarela <janne.juhani@gmail.com>
2015-01-04 19:05:38 +0200

    Update gruntfile, add tasks to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function(grunt) {
   // grunt.loadNpmTasks('grunt-contrib-copy');
   // grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['sass', 'copy', 'concat', 'uglify']);
   grunt.registerTask('default', ['copy', 'uglify', 'concat', 'watch']);
 
 }
