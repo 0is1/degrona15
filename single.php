5dee5c85d989116c0b63ae9279ad152978066745
Janne Saarela <janne.juhani@gmail.com>
2015-01-04 23:20:42 +0200

    Edit textdomain, uncomment comments_template

--- single.php
+++ single.php
@@ -31,12 +31,12 @@
 			<?php the_content(); ?>
 			</div>
 			<footer>
-				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
+				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'DeGrona15'), 'after' => '</p></nav>' )); ?>
 				<p><?php the_tags(); ?></p>
 			</footer>
-			<?php //do_action('foundationPress_post_before_comments'); ?>
-			<?php //comments_template(); ?>
-			<?php //do_action('foundationPress_post_after_comments'); ?>
+			<?php do_action('foundationPress_post_before_comments'); ?>
+			<?php comments_template(); ?>
+			<?php do_action('foundationPress_post_after_comments'); ?>
 		</article>
 	<?php endwhile;?>
 
