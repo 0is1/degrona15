e2b060cda40a8988e9caff5839d531212fc28ab4
Janne Saarela <janne.juhani@gmail.com>
2015-01-05 15:48:28 +0200

    Add custom searchform templates for navigation

---
+++ search/navigation-search-mobile.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="searchform-no-button" action="<?php echo home_url('/'); ?>">
+	<div class="collapse row input-border-radius">
+		<div class="small-12 column">
+			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'DeGrona15' ); ?>">
+		</div>
+	</div>
+</form>
