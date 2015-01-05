1b4af6444a99b77d97fad201ea3d706fea48e781
Janne Saarela <janne.juhani@gmail.com>
2015-01-05 15:47:27 +0200

    Add custom navigation templates

---
+++ navigation/off-canvas-menu.php
@@ -0,0 +1,6 @@
+<aside class="left-off-canvas-menu" aria-hidden="true">
+    <?php foundationPress_mobile_off_canvas(); ?>
+    <ul class="off-canvas-list search">
+      <li class="menu-item"><?php get_template_part( 'search/navigation-search-mobile' ); ?></li>
+    </ul>
+</aside>
