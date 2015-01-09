a27c5fe8a6d05e12c1f6eac7cb38c0d26a375f6c
Janne Saarela <janne.juhani@gmail.com>
2015-01-09 15:43:27 +0200

    Edit div size

--- search/navigation-search.php
+++ search/navigation-search.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="searchform-no-button" action="<?php echo home_url('/'); ?>">
 	<div class="collapse input-border-radius">
-		<div class="small-2 column right no-padding">
+		<div class="small-3 column right no-padding">
 			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'DeGrona15' ); ?>">
 		</div>
 	</div>
