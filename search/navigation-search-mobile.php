56d42c388ab24bfcba21e63662147639cb2af47d
Janne Saarela <janne.juhani@gmail.com>
2015-01-09 16:00:30 +0200

    Add css helper class

--- search/navigation-search-mobile.php
+++ search/navigation-search-mobile.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="searchform-no-button" action="<?php echo home_url('/'); ?>">
+<form role="search" method="get" class="searchform-no-button mobile" action="<?php echo home_url('/'); ?>">
 	<div class="collapse row input-border-radius">
 		<div class="small-12 column">
 			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'DeGrona15' ); ?>">
--- search/navigation-search.php
+++ search/navigation-search.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="searchform-no-button" action="<?php echo home_url('/'); ?>">
+<form role="search" method="get" class="searchform-no-button desktop" action="<?php echo home_url('/'); ?>">
 	<div class="collapse input-border-radius">
 		<div class="small-3 column right no-padding">
 			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'DeGrona15' ); ?>">
