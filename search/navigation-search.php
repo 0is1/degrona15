b56cc32d4b64e96fd844bdd509db52feedbd0abf
Janne Saarela <janne.juhani@gmail.com>
2015-01-10 20:43:36 +0200

    Fix navigation desktop search

--- search/navigation-search.php
+++ search/navigation-search.php
@@ -1,6 +1,6 @@
-<form role="search" method="get" class="searchform-no-button desktop" action="<?php echo home_url('/'); ?>">
+<form role="search" method="get" class="searchform-no-button desktop small-3 right" action="<?php echo home_url('/'); ?>">
 	<div class="collapse input-border-radius">
-		<div class="small-3 column right no-padding">
+		<div class="no-padding">
 			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'DeGrona15' ); ?>">
 		</div>
 	</div>
