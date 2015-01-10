cb178089f5a8b53097362551f990b9e4fc60724d
Janne Saarela <janne.juhani@gmail.com>
2015-01-10 16:56:15 +0200

    Add try search in 404-page

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php _e('File Not Found', 'DeGrona15'); ?></h1>
+				<h1 class="entry-title"><?php _e('Page Not Found', 'DeGrona15'); ?></h1>
 			</header>
 			<div class="entry-content">
 				<div class="error">
@@ -14,15 +14,17 @@
 				</div>
 				<p><?php _e('Please try the following:', 'DeGrona15'); ?></p>
 				<ul>
-					<li><?php _e('Check your spelling', 'DeGrona15'); ?></li>
-					<li><?php printf(__('Return to the <a href="%s">home page</a>', 'DeGrona15'), home_url()); ?></li>
-					<li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'DeGrona15'); ?></li>
+					<li><?php _e( 'Check your spelling', 'DeGrona15' ); ?></li>
+					<li><?php printf( __( 'Return to the <a href="%s">home page</a>', 'DeGrona15' ), home_url() ); ?></li>
+					<li><?php _e( 'Click the <a href="javascript:history.back()">Back</a> button', 'DeGrona15' ); ?></li>
+					<li><?php _e( 'Try search &darr;', 'DeGrona15' ); ?></li>
 				</ul>
+				<?php get_search_form(); ?>
 			</div>
 		</article>
 
 	</div>
 <?php do_action( 'degrona15_after_page_content' ); ?>
-	<?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 </div>
 <?php get_footer(); ?>
