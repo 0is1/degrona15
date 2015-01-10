6cdb4e735850a16bb52788cc778d5218e08d79a4
Janne Saarela <janne.juhani@gmail.com>
2015-01-10 14:03:21 +0200

    Add archive page #10

---
+++ archive.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+<div class="row">
+<?php do_action( 'degrona15_before_page_content' ); ?>
+<!-- Row for main content area -->
+	<div class="small-12 large-8 columns" role="main">
+
+	<?php if ( have_posts() ) : ?>
+
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php endwhile; ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+
+	<?php endif; // end have_posts() check ?>
+
+	<?php /* Display navigation to next/previous pages when applicable */ ?>
+	<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'DeGrona15' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'DeGrona15' ) ); ?></div>
+		</nav>
+	<?php } ?>
+
+	</div>
+	<?php do_action( 'degrona15_after_page_content' ); ?>
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
