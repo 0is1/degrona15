773f996fbd244028785dfc243b0cb5ac8c4403c9
Janne Saarela <janne.juhani@gmail.com>
2015-01-27 19:45:43 +0200

    Show only excerpt in content-lists (posts, archive, search results etc.) and add show more -button

--- content.php
+++ content.php
@@ -10,7 +10,8 @@
 		<?php degrona15_entry_meta(); ?>
 	</header>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading...', 'DeGrona15' ) ); ?>
+		<?php the_excerpt(); ?>
+		<p class="more button radius"><a href="<?php the_permalink();?>"><?php _e( 'Continue reading', 'DeGrona15' ); ?></a></p>
 	</div>
 	<footer>
 		<?php $tag = get_the_tags(); if (!$tag) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
